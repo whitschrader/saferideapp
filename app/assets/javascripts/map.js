081e57bb30dcab49cffd6e4a405cd51694391a03
Noah Kaplan <noahkaplan4@gmail.com>
2014-04-30 17:30:27 -0700

    removed map controls

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -113,7 +113,9 @@ $( window ).on('load', function() {
     var mapOptions = {
       // center: new google.maps.LatLng(-34.397, 150.644),
       zoom: 14,
-      panControl: false
+      panControl: false,
+      zoomControl: false,
+      mapTypeControl: false
     };
     
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
